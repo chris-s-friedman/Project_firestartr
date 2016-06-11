804b93b5a023f352b74b8016da564575228de4e5
Chris Friedman <chris.s.friedman@gmail.com>
2016-06-11 16:31:51 -0400

    added fileencoding arg

--- R/multmerge.R
+++ R/multmerge.R
@@ -1,5 +1,8 @@
-multmerge <- function(mypath){
+multmerge <- function(mypath, fileEncoding = ""){
   filenames=list.files(path=mypath, full.names=TRUE)
-  datalist = lapply(filenames, function(x){read.csv(file=x,header=T)})
+  datalist = lapply(filenames,
+                    function(x){read.csv(file=x,
+                                         header=T,
+                                         fileEncoding = fileEncoding)})
   Reduce(function(x,y) {rbind(x,y)}, datalist)
 }
